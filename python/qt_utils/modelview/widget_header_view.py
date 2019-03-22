6191da5bb3fb5e4465ffe1ee557184417a73bb7a
Matthew <mattalexshaw@gmail.com>
2019-03-22 23:15:09 +0000

    WIP: View which supports custom widgets in arbitrary locations within the geometry

--- python/qt_utils/modelview/widget_header_view.py
+++ python/qt_utils/modelview/widget_header_view.py
@@ -107,7 +107,17 @@ class FilterField(object):
             self.save()
 
 
+class HeaderWidget(QtWidgets.QWidget):
+    editingFinished = QtCore.Signal()
+    headerDataChanged = QtCore.Signal(int, object)  # Role, value
+
+    def on_header_data_changed(self, role, value):
+        pass
+
+
 class FilterLineEdit(QtWidgets.QLineEdit):
+    headerDataChanged = QtCore.Signal(int, object)  # Role, value
+
     def __init__(self, filter_field, parent=None):
         # type: (FilterField, QtWidgets.QWidget) -> None
         super(FilterLineEdit, self).__init__(parent)
@@ -115,10 +125,6 @@ class FilterLineEdit(QtWidgets.QLineEdit):
         self.filter_field = filter_field
         self.update_style()
         self.editingFinished.connect(self.on_editing_finished)
-
-    def mouseReleaseEvent(self, event):
-        # If middle click, clear the value
-        super(FilterLineEdit, self).mouseReleaseEvent(event)
 
     def update_style(self):
         if not self.filter_field.editable:
@@ -145,51 +151,53 @@ class FilterComboBoolean(QtWidgets.QComboBox):
         self.addItems(['No Filter', 'True', 'False'])
 
 
-class FilterHeader(QtWidgets.QHeaderView):
-    FilterTypeRole = QtCore.Qt.UserRole + 1
+class WidgetHeaderView(QtWidgets.QHeaderView):
+    HeaderDataRole = QtCore.Qt.UserRole + 1000
+    HeaderDataTypeRole = QtCore.Qt.UserRole + 1001
 
     def __init__(self, parent=None):
-        super(FilterHeader, self).__init__(QtCore.Qt.Horizontal, parent)
-        self.filters = []
+        super(WidgetHeaderView, self).__init__(QtCore.Qt.Horizontal, parent)
+        self._widgets = []
+        self._widget_type_mapping = {}
 
         self.setSectionsClickable(True)
         self.setHighlightSections(True)
-        self.setSectionsMovable(True)
         self.setDefaultAlignment(QtCore.Qt.AlignHCenter | QtCore.Qt.AlignTop)
 
-        self.sectionResized.connect(self.on_section_resized)
-        self.sectionMoved.connect(self.on_section_moved)
+    def set_widget_type(self, data_type, widget_cls):
+        # Convenience method for setting the widget type to use for particular
+        # data types. Data types are anything returned by the model for the
+        # HeaderDataTypeRole
+        self._widget_type_mapping[data_type] = widget_cls
+        for i, w in enumerate(self._widgets):
+            if self.model().headerData(i, self.orientation(), self.HeaderDataTypeRole) == data_type:
+                self._widgets[i] = self.create_header_widget(i)
 
-    def create_filter_field(self, logical_index):
-        # Can be subclassed for custom filter parsing
-        model = self.model()
-        name = model.headerData(logical_index, QtCore.Qt.Horizontal, QtCore.Qt.DisplayRole)
-        filter_type = model.headerData(logical_index, QtCore.Qt.Horizontal, self.FilterTypeRole)
-        editable = model.headerData(logical_index, QtCore.Qt.Horizontal, QtCore.Qt.EditRole)
-        filter_field = FilterField(name, filter_type, editable)
-        return filter_field
+    def set_default_widget(self, widget_cls):
+        self.set_widget_type(None, widget_cls)
 
-    def create_filter_widget(self, logical_index):
+    def create_header_widget(self, logical_index):
         # type: (int) -> QtWidgets.QWidget
         # Can be subclassed for custom widgets
-        filter_field = self.create_filter_field(logical_index)
-        # Connect editingFinished signal
+        model = self.model()
+        name = model.headerData(logical_index, QtCore.Qt.Horizontal, QtCore.Qt.DisplayRole)
+        data_type = model.headerData(logical_index, QtCore.Qt.Horizontal, self.HeaderDataRole)
+        editable = model.headerData(logical_index, QtCore.Qt.Horizontal, QtCore.Qt.EditRole)
+        filter_field = FilterField(name, data_type, editable)
         widget = FilterLineEdit(filter_field, self)
-        widget.setGeometry(self.get_widget_geometry(logical_index))
-        widget.show()
-        self.filters.append(widget)
+        # Can ensure the widget stays up to date with the source model
+        # self.headerDataChanged.connect()
         return widget
 
-    def sizeHint(self):
-        # type: () -> QtCore.QSize
-        size = super(FilterHeader, self).size()
-        return QtCore.QSize(size.width(), 50)
-
-    def showEvent(self, event):
-        self.filters = []
-        for i in range(self.count()):
-            self.create_filter_widget(i)
-        super(FilterHeader, self).showEvent(event)
+    def get_header_geometry(self, logical_index):
+        # type: (int) -> QtCore.QRect
+        half_height = self.height() * 0.5
+        return QtCore.QRect(
+            self.sectionViewportPosition(logical_index) + MARGIN,
+            0,
+            self.sectionSize(logical_index) - MARGIN,
+            half_height,
+        )
 
     def get_widget_geometry(self, logical_index):
         # type: (int) -> QtCore.QRect
@@ -201,27 +209,62 @@ class FilterHeader(QtWidgets.QHeaderView):
             half_height,
         )
 
-    def on_section_resized(self, logical_index):
-        for i in range(self.visualIndex(logical_index), self.count()):
-            logical = self.logicalIndex(i)
-            self.filters[logical].setGeometry(self.get_widget_geometry(logical))
+    def sizeHint(self):
+        # type: () -> QtCore.QSize
+        size = super(WidgetHeaderView, self).size()
+        return QtCore.QSize(size.width(), 50)
 
-    def on_section_moved(self, logical, old_visual_index, new_visual_index):
-        for i in range(min(old_visual_index, new_visual_index), self.count()):
-            logical = self.logicalIndex(i)
-            self.filters[logical].setGeometry(self.get_widget_geometry(logical))
+    def paintSection(self, painter, rect, logical_index):
+        # type: (QtGui.QPainter, QtCore.QRect, int) -> None
+        # Render the filter widget in the header widget rect
+        painter.save()
+        widget_geo = self.get_widget_geometry(logical_index)
+        painter.translate(widget_geo.topLeft())
+        widget = self._widgets[logical_index]
+        widget.resize(widget_geo.size())
+        widget.render(painter, QtCore.QPoint())
+        painter.restore()
 
-    def fix_widget_positions(self, start=0):
-        for i in range(start, self.count()):
-            self.filters[i].setGeometry(self.get_widget_geometry(i))
+        # Render the original header section in the remaining area
+        header_geo = self.get_header_geometry(logical_index)
+        super(WidgetHeaderView, self).paintSection(painter, header_geo, logical_index)
 
+    def mouseReleaseEvent(self, event):
+        clickable = self.sectionsClickable()
+        # Check if the release happens in the region of a widget and if so,
+        # display the widget
+        index = self.logicalIndexAt(event.pos())
+        if 0 <= index < self.count():
+            rect = self.get_widget_geometry(index)
+            if rect.contains(event.pos()):
+                widget = self._widgets[index]
+                widget.setGeometry(rect)
+                # TODO: This won't work for everything, will require a base class
+                widget.editingFinished.connect(widget.close)
+                widget.show()
+                widget.setFocus(QtCore.Qt.MouseFocusReason)
+                # Prevent interaction with the widget region from
+                # triggering a section click
+                self.setSectionsClickable(False)
+        super(WidgetHeaderView, self).mouseReleaseEvent(event)
+        if self.sectionsClickable() != clickable:
+            self.setSectionsClickable(clickable)
 
-class View(QtWidgets.QTableView):
-    def scrollContentsBy(self, dx, dy):
-        super(View, self).scrollContentsBy(dx, dy)
-        hheader = self.horizontalHeader()
-        if dx != 0:
-            hheader.fix_widget_positions()
+    def resizeEvent(self, event):
+        super(WidgetHeaderView, self).resizeEvent(event)
+        # Because we're manually setting the widget geometry to make it visible,
+        # it will not respond correctly to the viewport being modified. Ensure
+        # the widget is hidden and loses any focus.
+        for widget in self._widgets:
+            if widget.hasFocus():
+                widget.clearFocus()
+            if widget.isVisible():
+                widget.close()
+
+    def showEvent(self, event):
+        # Create a widget for each column
+        self._widgets = [self.create_header_widget(i) for i in range(self.count())]
+        super(WidgetHeaderView, self).showEvent(event)
 
 
 if __name__ == '__main__':
@@ -248,9 +291,10 @@ if __name__ == '__main__':
         QtGui.QStandardItem('female'),
         QtGui.QStandardItem('dunlop'),
     ])
-    view = View()
-    header = FilterHeader()
+    view = QtWidgets.QTableView()
+    header = WidgetHeaderView()
     view.setHorizontalHeader(header)
+    view.setSortingEnabled(True)
     view.setModel(model)
     view.show()
     # view.resizeColumnsToContents()

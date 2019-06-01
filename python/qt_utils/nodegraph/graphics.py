37338944a2dbb932f899658c2b40c81dbe163a55
Matthew <mattalexshaw@gmail.com>
2019-06-01 20:42:01 +0100

    use parent-child instead of group, add drag drop

--- python/qt_utils/nodegraph/graphics.py
+++ python/qt_utils/nodegraph/graphics.py
@@ -9,13 +9,17 @@ FLAG_STATES = QtWidgets.QStyle.State_MouseOver | QtWidgets.QStyle.State_Selected
 
 
 class PortItem(QtWidgets.QGraphicsItem):
-    Radius = 6
+    Radius = 7
 
     def __init__(self, port, parent=None):
         # type: (api.Port, QtWidgets.QGraphicsItem) -> None
         super(PortItem, self).__init__(parent)
         self.setAcceptHoverEvents(True)
+        self.setAcceptDrops(True)
+        self.setAcceptedMouseButtons(QtCore.Qt.LeftButton)
+        self.setCursor(QtCore.Qt.OpenHandCursor)
         self._port = port
+        self._drag_over = False
 
     @property
     def port(self):
@@ -29,7 +33,7 @@ class PortItem(QtWidgets.QGraphicsItem):
             -PortItem.Radius,
             PortItem.Radius * 2,
             PortItem.Radius * 2,
-        )
+            )
 
     def shape(self):
         # type: () -> QtGui.QPainterPath
@@ -40,7 +44,9 @@ class PortItem(QtWidgets.QGraphicsItem):
     def paint(self, painter, option, widget):
         # type: (QtGui.QPainter, QtWidgets.QStyleOptionGraphicsItem , QtWidgets.QWidget) -> None
         painter.save()
-        if option.state & FLAG_STATES:
+        if self._drag_over:
+            colour = QtGui.QColor("#FFFFDD")
+        elif option.state & FLAG_STATES:
             colour = QtGui.QColor("#FFDDDD")
         else:
             colour = QtGui.QColor("#DDFFDD")
@@ -48,8 +54,53 @@ class PortItem(QtWidgets.QGraphicsItem):
         painter.drawEllipse(self.boundingRect())
         painter.restore()
 
+    def dragEnterEvent(self, event):
+        event.setAccepted(True)
+        self._drag_over = True
+        # if event.mimeData().hasColor():
+        #     self._drag_over = True
+        #     self.update()
+        # else:
+        #     event.setAccepted(False)
 
-class _NodeItem(QtWidgets.QGraphicsItem):
+    def dragLeaveEvent(self, event):
+        self._drag_over = False
+        self.update()
+
+    def dropEvent(self, event):
+        mime = event.mimeData()
+        print("{}.{}->{}.{}".format(
+            mime.data("node"),
+            mime.data("port"),
+            self._port.node.name,
+            self._port.name,
+        ))
+        self._drag_over = False
+        # if event.mimeData().hasColor():
+        #     color = qvariant_cast < QColor > (event->mimeData()->colorData());
+        self.update()
+
+    def mousePressEvent(self, event):
+        self.setCursor(QtCore.Qt.ClosedHandCursor)
+
+    def mouseReleaseEvent(self, event):
+        self.setCursor(QtCore.Qt.OpenHandCursor)
+
+    def mouseMoveEvent(self, event):
+        if QtCore.QLineF(event.screenPos(), event.buttonDownScreenPos(QtCore.Qt.LeftButton)).length() < QtWidgets.QApplication.startDragDistance():
+            return
+
+        drag = QtGui.QDrag(event.widget())
+        mime = QtCore.QMimeData()
+        mime.setData("node", self._port.node.name)
+        mime.setData("port", self._port.name)
+
+        drag.setMimeData(mime)
+        drag.exec_()
+        self.setCursor(QtCore.Qt.OpenHandCursor)
+
+
+class NodeItem(QtWidgets.QGraphicsItem):
     Width = 150
     HeaderHeight = 30
     AttrHeight = 20
@@ -57,10 +108,26 @@ class _NodeItem(QtWidgets.QGraphicsItem):
 
     def __init__(self, node, parent=None):
         # type: (api.Node, QtWidgets.QGraphicsItem) -> None
-        super(_NodeItem, self).__init__(parent)
+        super(NodeItem, self).__init__(parent)
         self.setAcceptHoverEvents(True)
+        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
         self._node = node
         # TODO: Enable paint caching?
+
+        height = NodeItem.HeaderHeight + NodeItem.AttrHeight // 2
+        inputs, outputs = node.list_inputs(), node.list_outputs()
+        for input_port, output_port in zip_longest(inputs, outputs):
+            if input_port is not None:
+                port = PortItem(input_port, parent=self)
+                port.setPos(0, height)
+            if output_port is not None:
+                port = PortItem(output_port, parent=self)
+                port.setPos(NodeItem.Width, height)
+            height += NodeItem.AttrHeight
+
+    @property
+    def node(self):
+        return self._node
 
     def boundingRect(self):
         # type: () -> QtCore.QRect
@@ -68,8 +135,8 @@ class _NodeItem(QtWidgets.QGraphicsItem):
         return QtCore.QRect(
             0,
             0,
-            _NodeItem.Width,
-            num_attrs * _NodeItem.AttrHeight + self.HeaderHeight,
+            NodeItem.Width,
+            num_attrs * NodeItem.AttrHeight + self.HeaderHeight,
         )
 
     def shape(self):
@@ -93,7 +160,7 @@ class _NodeItem(QtWidgets.QGraphicsItem):
         painter.drawRect(rect)
 
         fm = QtGui.QFontMetrics(painter.font())
-        text_height = _NodeItem.HeaderHeight - (_NodeItem.HeaderHeight - fm.height()) * 0.5
+        text_height = NodeItem.HeaderHeight - (NodeItem.HeaderHeight - fm.height()) * 0.5
         painter.drawText(
             (rect.width() - fm.width(self._node.name)) * 0.5,
             text_height,
@@ -101,12 +168,12 @@ class _NodeItem(QtWidgets.QGraphicsItem):
         )
 
         # Attributes
-        height = _NodeItem.HeaderHeight
-        height_offset = _NodeItem.AttrHeight - (_NodeItem.AttrHeight - fm.height()) * 0.5
-        output_offset = rect.width() - _NodeItem.Padding - PortItem.Radius
+        height = NodeItem.HeaderHeight
+        height_offset = NodeItem.AttrHeight - (NodeItem.AttrHeight - fm.height()) * 0.5
+        output_offset = rect.width() - NodeItem.Padding - PortItem.Radius
         inputs, outputs = self._node.list_inputs(), self._node.list_outputs()
         for input_port, output_port in zip_longest(inputs, outputs):
-            painter.drawLine(0, height, _NodeItem.Width, height)
+            painter.drawLine(0, height, NodeItem.Width, height)
             if input_port is not None:
                 painter.drawText(
                     self.Padding + PortItem.Radius,
@@ -119,70 +186,37 @@ class _NodeItem(QtWidgets.QGraphicsItem):
                     height + height_offset,
                     output_port.name,
                 )
-            height += _NodeItem.AttrHeight
+            height += NodeItem.AttrHeight
 
         painter.restore()
 
 
-class NodeItem(QtWidgets.QGraphicsItemGroup):
-    def __init__(self, node, parent=None):
-        # type: (api.Node, QtWidgets.QGraphicsItem) -> None
-        super(NodeItem, self).__init__(parent)
-        self.setAcceptHoverEvents(True)
-        self.setFlag(QtWidgets.QGraphicsItem.ItemIsMovable, True)
-        self._node = node
-
-        self._item = _NodeItem(node)
-        self.addToGroup(self._item)
-
-        height = _NodeItem.HeaderHeight + _NodeItem.AttrHeight // 2
-        inputs, outputs = node.list_inputs(), node.list_outputs()
-        for input_port, output_port in zip_longest(inputs, outputs):
-            if input_port is not None:
-                port = PortItem(input_port, parent=self)
-                port.setPos(0, height)
-                self.addToGroup(port)
-            if output_port is not None:
-                port = PortItem(output_port, parent=self)
-                port.setPos(_NodeItem.Width, height)
-                self.addToGroup(port)
-            height += _NodeItem.AttrHeight
-
-    @property
-    def node(self):
-        # type: () -> api.Node
-        return self._node
-
-
-class Widget(QtWidgets.QWidget):
+class Widget(QtWidgets.QGraphicsView):
     def __init__(self, parent=None):
         # type: (QtWidgets.QWidget) -> None
         super(Widget, self).__init__(parent)
 
         # ----- Widgets -----
 
-        self.scene = QtWidgets.QGraphicsScene(self)
+        self._scene = QtWidgets.QGraphicsScene(self)
 
-        node = api.Node("Node", "NodeItem")
+        node = api.Node("Node", "NodeItem1")
         node.add_input_port("input_0")
         node.add_input_port("input_1")
         node.add_output_port("output_0")
         node.add_output_port("output_1")
         node.add_output_port("output_2")
-        self.scene.addItem(NodeItem(node))
+        self._scene.addItem(NodeItem(node))
 
-        self.view = QtWidgets.QGraphicsView()
-        self.view.setScene(self.scene)
+        node = api.Node("Node", "NodeItem2")
+        node.add_input_port("input_0")
+        node.add_input_port("input_1")
+        node.add_output_port("output_0")
+        node.add_output_port("output_1")
+        node.add_output_port("output_2")
+        self._scene.addItem(NodeItem(node))
 
-        # ----- Layout -----
-
-        main_layout = QtWidgets.QVBoxLayout()
-        main_layout.addWidget(self.view)
-        self.setLayout(main_layout)
-
-        # ----- Connections -----
-
-        # ----- Initialise -----
+        self.setScene(self._scene)
 
 
 if __name__ == '__main__':
